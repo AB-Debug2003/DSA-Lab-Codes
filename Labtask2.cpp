1cf7e95ba5380349386e928512d5457c2add9588
AB-Debug2003 <abdesigndesk@gmail.com>
2022-10-26 13:30:28 +0500

    n

---
+++ Labtask2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int sz;
+   int randArray[10];
+   for(int i=0; i<10; i++)
+   {
+        randArray[i]=rand()%100;
+   }
+    
+   cout<<"\nElements of the array::"<<endl;
+  
+   for(int i=0; i<10; i++)
+   {
+        cout << randArray[i]<<endl;
+   }
+      
+   return 0;
+}
