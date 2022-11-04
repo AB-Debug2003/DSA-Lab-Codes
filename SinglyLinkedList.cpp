597755d28e256ccd250861df28364b30da83aac6
AB-Debug2003 <abdesigndesk@gmail.com>
2022-11-04 20:02:48 +0500

    comit

--- SinglyLinkedList.cpp
+++ SinglyLinkedList.cpp
@@ -6,6 +6,40 @@ class Node{
     int key;
     int data;
     Node* next;
+
+public:
+    Node()
+    {
+        key = 0;
+        data = 0;
+        next = NULL;
+    }
+
+    Node(int key, int data)
+    {
+        this->key = key;
+        this->data = data;
+    }
+};
+
+class SinglyLinkedList{
+    Node* head;
+
+public:
+    SinglyLinkedList()
+    {
+        head = NULL;
+    }
+
+    SinglyLinkedList(Node *n)
+    {
+        head = n;
+    }
+
+    Node* nodeExist(int k)
+    {
+        
+    }
 };
 
 int main(){
