03aff5624defe92f44593a1ba8a966874ca08bb7
AB-Debug2003 <abdesigndesk@gmail.com>
2022-11-05 22:39:15 +0500

    finalized

--- SinglyLinkedList.cpp
+++ SinglyLinkedList.cpp
@@ -92,7 +92,8 @@ public:
         // Check if node of same key value already exists
         if (nodeExist(n->key)!=NULL)
         {
-            cout << "Node already exists at Position " << n->key << ". Try with a different Position value." <<endl;
+            cout << "Node already exists at Position " << n->key <<
+            ". Try with a different Position value." <<endl;
         }
 
         else
