c2fb98a5b8f11dcb6fcbdccfdcb460ce59de2794
AB-Debug2003 <abdesigndesk@gmail.com>
2022-11-27 13:19:46 +0500

    f

--- sorting.cpp
+++ sorting.cpp
@@ -2,46 +2,52 @@
 #include<string>
 using namespace std;
 
-int main(){
-    int arr[5] = {};
-    cout << "Enter 5 elemnts of array: " << endl;
-    for (int i = 0; i < 5; i++)
-    {
-        cin >> arr[i];
-    }
-    
-    cout << "Unsorted Array: " << endl;
-    for (int i = 0; i < 5; i++)
-    {
-        cout << arr[i] << endl;
-    }  
-
-    cout << endl;
-
-    for (int i = 0; i < 4; i++)
+void selectionSort(int arr[]) 
+{
+    for (int i = 0; i < 4; i++) 
     {
         int min = i;
-        for (int j = i+1; j < 5; i++)
+        for (int j = i + 1; j < 5; j++) 
         {
-            if (arr[j] < arr[min])
+            if (arr[j] < arr[min]) 
             {
                 min = j;
             }
         }
-        
-        if (min != i)
+        if (min != i) 
         {
             int temp = arr[min];
             arr[min] = arr[i];
             arr[i] = temp;
         }
     }
+}
 
-    cout << "Sorted Array: " << endl;
+int main(){
+    int myarr[5];
+    cout << "Enter 5 Elements of array: " << endl;
     for (int i = 0; i < 5; i++)
     {
-        cout << arr[i] << endl;
+        cin >> myarr[i];
     }
     
+    cout << "Unsorted Array: " << endl;
+    cout << "[";
+    for (int i = 0; i < 5; i++)
+    {
+        cout << myarr[i] << ",";
+    }
+    cout << "]" << endl;
+
+    selectionSort(myarr);
+    
+    cout << "Sorted Array: " << endl;
+    cout << "[";
+    for (int i = 0; i < 5; i++)
+    {
+        cout << myarr[i] << ",";
+    }
+    cout << "]" << endl;
+
     return 0;
 }
