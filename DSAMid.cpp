b54990ad8d641babb321dbc8da5239d5b9deb5f4
AB-Debug2003 <abdesigndesk@gmail.com>
2022-12-14 00:06:53 +0500

    m

--- DSAMid.cpp
+++ DSAMid.cpp
@@ -4,45 +4,72 @@ using namespace std;
 
 class Stack {
 public:
+	int stacksize = 5;
 	int top;
-	int arr[5];
-	Stack() {
+	int arr[];
+	Stack() 
+	{
 		top = -1;
-		for (int i = 0; i < 5; i++) {
+		int arr[stacksize];
+		for (int i = 0; i < stacksize; i++) 
+		{
 			arr[i] = 0;
 		}
 	}
 
+	void setSize()
+	{
+		cout << "Enter Stack size: ";
+		cin >> stacksize;
+		arr[stacksize-1];
+		Stack();
+	}
+
 	bool isEmpty() {
 		if (top == -1)
+		{
 			return true;
-		else
-			return false;
-	}
-
-	bool isFull() {
-		if (top == 4)
-			return true;
-		else
-			return false;
-	}
-
-	void push(int val) {
-		if (isFull()) {
-			cout << "stack overflow" << endl;
 		}
-		else {
+		else
+		{
+			return false;
+		}
+	}
+
+	bool isFull() 
+	{
+		if (top == stacksize-1)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	void push(int val) 
+	{
+		if (isFull()) 
+		{
+			cout << "Error! Stack overflow" << endl;
+		}
+		else 
+		{
 			top++;
 			arr[top] = val;
 		}
 	}
 
-	int pop() {
-		if (isEmpty()) {
+	int pop() 
+	{
+		if (isEmpty()) 
+		{
 			cout << "stack underflow" << endl;
 			return 0;
 		}
-		else {
+		else 
+		{
 			int popValue = arr[top];
 			arr[top] = 0;
 			top--;
@@ -50,22 +77,24 @@ public:
 		}
 	}
 
-	int count() {
+	int count() 
+	{
 		return (top + 1);
 	}
 
-	void display() {
-		cout << "All values in the Stack are " << endl;
-		for (int i = 4; i >= 0; i--) {
-			cout << arr[i] << endl;
+	void display() 
+	{
+		cout << endl;
+		for (int i = stacksize-1; i >= 0; i--) {
+			cout << "[" << arr[i] << "]" << endl;
 		}
 	}
 	
 	void bubbleSort()
 	{
-		for (int i = 0; i < top; i++)
+		for (int i = 0; i < top+1; i++)
 		{
-			for (int j = 0; j < (top - i - 1); j++)
+			for (int j = 0; j < ((top+1) - i - 1); j++)
 			{
 				if (arr[j] > arr[j + 1])
 				{
@@ -79,8 +108,6 @@ public:
 
 	void mergeSort(int left, int right) 
 	{
-		left = 0;
-		right = top;
 		if (left < right) 
 		{
 			int mid = (left + right) / 2;
@@ -292,29 +319,15 @@ public:
         }
     }
 
-	void mergeSort() 
+	void merge(int arr[], int l, int m, int r, int size)
 	{
-		if (0 < total) 
-		{
-			int mid = (0 + total) / 2;
-			
-			mergeSort();
-			mergeSort();
+		int i = l;
+		int j = m + 1;
+		int k = l;
 
-			// merge
-			merge(arr, 0, mid, total);
-		}
-	}
+		int temp[size];
 
-	void merge(int* arr, int left, int mid, int right) 
-	{
-		int i = left;
-		int j = mid + 1;
-		int k = left;
-
-		int temp[5];
-
-		while (i <= mid && j <= right) 
+		while (i <= m && j <= r) 
 		{
 			if (arr[i] <= arr[j]) 
 			{
@@ -328,254 +341,390 @@ public:
 				j++;
 				k++;
 			}
-
 		}
 
-		while (i <= mid) 
+		while (i <= m) 
 		{
 			temp[k] = arr[i];
 			i++;
 			k++;
 		}
 
-		while (j <= right) 
+		while (j <= r) 
 		{
 			temp[k] = arr[j];
 			j++;
 			k++;
 		}
 
-		for (int p = left; p <= right; p++) 
+		for (int p = l; p <= r; p++) 
 		{
 			arr[p] = temp[p];
 		}
 	}
+
+	void mergeSort(int arr[], int l, int r, int size)
+	{
+		if (l < r) 
+		{
+			int m = (l + r) / 2;
+			mergeSort(arr, l, m, size);
+			mergeSort(arr, m + 1, r, size);
+			merge(arr, l, m, r, size);
+		}
+	}
 };
 
+void linearSearch(int a[], int n, int size) {
+  int temp = -1;
+
+  for (int i = 0; i < size; i++) {
+    if (a[i] == n) {
+      cout << "Element found at position: " << i + 1 << endl;
+      temp = 0;
+      break;
+    }
+  }
+
+  if (temp == -1) {
+    cout << "No Element Found" << endl;
+  }
+
+}
+
+int binarySearch(int arr[], int left, int right, int x) 
+{
+	while (left <= right) 
+	{
+    	int mid = left + (right - left) / 2;
+
+		if (arr[mid] == x) 
+		{
+			return mid;
+		} 
+		else if (arr[mid] < x) 
+		{
+			left = mid + 1;
+		} 
+		else 
+		{
+			right = mid - 1;
+		}
+  	}
+
+  	return -1;
+}
+
 int main(){
-	int option = 'y';
-	int data_struct;
+	int option = 1;
 	do
 	{
-		Stack* s1 = new Stack;
-		ArrayList* a = new ArrayList;
-		do
-		{
-			cout << "Select a data structure of your choice or 0 to exit menu" << endl;
-			cout << "1. Stack" << endl;
-			cout << "2. List" << endl << endl;
-			cin >> data_struct;
-			switch (data_struct)
-			{
-				case 0:
-					break;
-					
-				case 1:
-					{	
-						int stackInput, stackValue;
-						cout << "Stack Selected successfully!" << endl;
-						do
-						{
-							cout << "Select Option number. Enter 0 to exit." << endl;
-							cout << "1. Push()" << endl;
-							cout << "2. Pop()" << endl;
-							cout << "3. isEmpty()" << endl;
-							cout << "4. isFull()" << endl;
-							cout << "5. count()" << endl;
-							cout << "6. display()" << endl;
-							cout << "7. Clear Screen" << endl << endl;
-							cin >> stackInput;
-							switch (stackInput)
-							{
-								case 0:
-									break;
-
-								case 1:
-									cout << "------Push Function-----" << endl;
-									cout << "Enter a value to push: ";
-									cin >> stackValue;
-									s1->push(stackValue);
-									break;
-
-								case 2:
-									cout << "-----Pop Function-----" << endl;
-									cout << "Enter a value to pop: ";
-									cout << "Poped Value:" << s1->pop();
-									break;
-
-								case 3:
-									if (s1->isEmpty())
-									{
-										cout << "Stack is Empty!" << endl;
-									}
-									else
-									{
-										cout << "Stack is not Empty!" << endl;
-									}
-									break;
-
-								case 4:
-									if (s1->isFull())
-									{
-										cout << "Stack is full!" << endl;
-									}
-									else
-									{
-										cout << "Stack is not Full!" << endl;
-									}
-									break;
-
-								case 5:
-									cout << "Count = " << s1->count();
-									break;
-
-								case 6:
-									s1->display();
-									break;
-
-								case 7:
-									system("cls");
-									break;
-
-								default:
-									cout << "Error! Enter correct option number" << endl;
-									break;
-							}
-						}while(stackInput != 0);
-					}
-					break;
-
-				case 2:
-					{
-						int listInput, listValue, newValue;
-						cout << "List Selected successfully!" << endl;
-						do
-						{
-							cout << "Select Option number. Enter 0 to exit." << endl;
-							cout << "1. Insert()" << endl;
-							cout << "2. Delete()" << endl;
-							cout << "3. Find()" << endl;
-							cout << "4. Update()" << endl;
-							cout << "5. Clear List()" << endl;
-							cout << "6. Display()" << endl;
-							cout << "7. Clear Screen" << endl << endl;
-							cin >> listInput;
-							switch (listInput)
-							{
-								case 0:
-									break;
-
-								case 1:
-									cout << "----Insert Function----" << endl;
-									cout << "Enter a value to insert:";
-									cin >> listValue;
-									a->insert(listValue);
-									break;
-
-								case 2:
-									cout << "----Delete Function----" << endl;
-									cout << "Enter a value to delete:";
-									cin >> listValue;
-									a->remove(listValue);
-									break;
-
-								case 3:
-									cout << "----Find Function----" << endl;
-									cout << "Enter a value to find:";
-									cin >> listValue;
-									if (a->find(listValue) != -1)
-									{
-										cout << "Value Found at position " << a->find(listValue) + 1 << endl;
-									}
-									else
-									{
-										cout << "Value Not Found!" << endl;
-									}
-									break;
-
-								case 4:
-									cout << "----Update Function----" << endl;
-									cout << "Enter a value to update:";
-									cin >> listValue;
-									cout << "Enter new Value: ";
-									cin >> newValue;
-									a->update(listValue, newValue);
-									break;
-
-								case 5:
-									cout << "----Clear List Function----" << endl;
-									a->clear();
-									break;
-
-								case 6:
-									cout << "----Display Function----" << endl;
-									a->display();
-									break;
-
-								case 7:
-									system("cls");
-
-								default:
-									break;
-							}
-						}while(listInput != 0);
-					}
-					break;
-
-					default:
-						cout << "Enter a valid Option Number" << endl;
-						break;
-			}
-		}while(data_struct != 0);
+		int data_struct;
 		
+
+		cout << "\nSelect a data structure of your choice. Enter 0 to go Sorting Menu." << endl;
+		cout << "1. Stack" << endl;
+		cout << "2. List" << endl << endl;
+		cin >> data_struct;
+		Stack* s1 = new Stack();
+		ArrayList* a = new ArrayList;
+		switch (data_struct)
+		{
+			case 0:
+				break;
+				
+			case 1:	
+				int stackInput, stackValue;
+				cout << "---------------------------" << endl;
+				cout << "Stack Selected successfully" << endl;
+				cout << "---------------------------" << endl;
+				s1->setSize();
+				do
+				{
+					cout << "\nSelect Option number. Enter 0 to go to Sorting menu." << endl;
+					cout << "1. Push()" << endl;
+					cout << "2. Pop()" << endl;
+					cout << "3. isEmpty()" << endl;
+					cout << "4. isFull()" << endl;
+					cout << "5. count()" << endl;
+					cout << "6. display()" << endl;
+					cout << "7. Clear Screen" << endl << endl;
+					cin >> stackInput;
+					cout << endl;
+					switch (stackInput)
+					{
+						case 0:
+							break;
+
+						case 1:
+							cout << "--Push Function--" << endl;
+							cout << "Enter a value to push: ";
+							cin >> stackValue;
+							s1->push(stackValue);
+							break;
+
+						case 2:
+							cout << "--Pop Function--" << endl;
+							cout << "Enter a value to pop: ";
+							cout << "Poped Value:" << s1->pop();
+							break;
+
+						case 3:
+							if (s1->isEmpty())
+							{
+								cout << "Stack is Empty!" << endl;
+							}
+							else
+							{
+								cout << "Stack is not Empty!" << endl;
+							}
+							break;
+
+						case 4:
+							if (s1->isFull())
+							{
+								cout << "Stack is full!" << endl;
+							}
+							else
+							{
+								cout << "Stack is not Full!" << endl;
+							}
+							break;
+
+						case 5:
+							cout << "Count = " << s1->count();
+							break;
+
+						case 6:
+							cout << "--Stack Display--" << endl;
+							s1->display();
+							break;
+
+						case 7:
+							system("cls");
+							break;
+
+						default:
+							cout << "Error! Enter correct option number" << endl;
+							break;
+					}
+				}while(stackInput != 0);
+				break;
+
+			case 2:
+				int listInput, listValue, newValue;
+				cout << "--------------------------" << endl;
+				cout << "List Selected successfully" << endl;
+				cout << "--------------------------" << endl;
+				do
+				{
+					cout << "\nSelect Option number. Enter 0 to exit." << endl;
+					cout << "1. Insert()" << endl;
+					cout << "2. Delete()" << endl;
+					cout << "3. Find()" << endl;
+					cout << "4. Update()" << endl;
+					cout << "5. Clear List()" << endl;
+					cout << "6. Display()" << endl;
+					cout << "7. Clear Screen" << endl << endl;
+					cin >> listInput;
+					switch (listInput)
+					{
+						case 0:
+							break;
+
+						case 1:
+							cout << "--Insert Function--" << endl;
+							cout << "Enter a value to insert:";
+							cin >> listValue;
+							a->insert(listValue);
+							break;
+
+						case 2:
+							cout << "--Delete Function--" << endl;
+							cout << "Enter a value to delete:";
+							cin >> listValue;
+							a->remove(listValue);
+							break;
+
+						case 3:
+							cout << "--Find Function--" << endl;
+							cout << "Enter a value to find:";
+							cin >> listValue;
+							if (a->find(listValue) != -1)
+							{
+								cout << "Value Found at position " << a->find(listValue) + 1 << endl;
+							}
+							else
+							{
+								cout << "Value Not Found!" << endl;
+							}
+							break;
+
+						case 4:
+							cout << "--Update Function--" << endl;
+							cout << "Enter a value to update:";
+							cin >> listValue;
+							cout << "Enter new Value: ";
+							cin >> newValue;
+							a->update(listValue, newValue);
+							break;
+
+						case 5:
+							cout << "--Clear List Function--" << endl;
+							a->clear();
+							break;
+
+						case 6:
+							cout << "--Display Function--" << endl;
+							a->display();
+							break;
+
+						case 7:
+							system("cls");
+
+						default:
+							break;
+					}
+				}while(listInput != 0);
+			break;
+
+			default:
+				cout << "Enter a valid Option Number" << endl;
+				break;
+		}
+
 		int sortOption;
 		do
 		{
-			cout << "Select a sorting method to apply" << endl;
-			cout << "1. Merge Sort" << endl;
-			cout << "2. Bubble Sort" << endl << endl;
+			cout << "\nEnter an option. Enter 0 to go to Searching Menu" << endl;
+			cout << "1. Bubble Sort" << endl;
+			cout << "2. Merge Sort" << endl << endl;
 			cin >> sortOption;
 			switch (sortOption)
 			{
-				case 0:
-					break;
-				
-				case 1:
-					cout << "----Merge Sort----" << endl;
-					if (data_struct == 1)
-					{
-						s1->mergeSort(0,5);
-					}
-					else
-					{
-						a->mergeSort();
-					}
-					break;
-				
-				case 2:
-					cout << "----Bubble Sort----" << endl;
-					if (data_struct == 1)
-					{
-						s1->bubbleSort();
-					}
-					else
-					{
-						a->bubbleSort();
-					}
-					break;
-					
-				default:
-					cout << "Enter a valid option number" << endl;
-					break;
-			}
+			case 0:
+				break;
+			
+			case 1:
+				cout << "------------------------" << endl;
+				cout << "Bubble Sorting Your Data" << endl;
+				cout << "------------------------" << endl;
+				if (data_struct == 1)
+				{
+					s1->bubbleSort();
+					cout << "Data Sorted!" << endl;
+					s1->display();
+				}
+				else
+				{
+					a->bubbleSort();
+					cout << "Data Sorted" << endl;
+					a->display();
+				}
+				break;
+
+			case 2:
+				cout << "-----------------------" << endl;
+				cout << "Merge Sorting Your Data" << endl;
+				cout << "-----------------------" << endl;
+				if (data_struct == 1)
+				{
+					s1->mergeSort(0, s1->top);
+					cout << "Data Sorted!" << endl;
+					s1->display();
+				}
+				else
+				{
+					a->mergeSort(a->arr, 0, a->total-1, a->total);
+					cout << "Data Sorted" << endl;
+					a->display();
+				}
+				break;
+
+			default:
+				break;
+			}			
 		} while (sortOption != 0);
+
+		int searchOption;
+		int num;
+		do
+		{
+			cout << "\nEnter an option. Enter 0 to go to main menu" << endl;
+			cout << "1. Linear Search" << endl;
+			cout << "2. Binary Search" << endl;
+			cin >> searchOption;
+			switch (searchOption)
+			{
+			case 0:
+				break;
+			
+			case 1:
+				cout << "----------------" << endl;
+				cout << "Linear Searching" << endl;
+				cout << "----------------" << endl;
+				cout << "Enter a Value to search:";
+				cin >> num;
+				if (data_struct == 1)
+				{
+					linearSearch(s1->arr, num, s1->top+1);
+				}
+				else
+				{
+					linearSearch(a->arr, num, a->total);
+				}
+				break;
+
+			case 2:
+				cout << "----------------" << endl;
+				cout << "Binary Searching" << endl;
+				cout << "----------------" << endl;
+				cout << "Enter a Value to search:";
+				cin >> num;
+				if (data_struct == 1)
+				{
+					int output = binarySearch(s1->arr, 0, s1->top+1, num);
+					if (output == -1) 
+					{
+						cout << "No Match Found" << endl;
+					} 
+					else 
+					{
+						cout << "Match found at position: " << output << endl;
+					}
+				}
+				else
+				{
+					int output = binarySearch(a->arr, 0, a->total, num);
+					if (output == -1) 
+					{
+						cout << "No Match Found" << endl;
+					} 
+					else 
+					{
+						cout << "Match found at position: " << output+1 << endl;
+					}
+					
+				}
+				break;
+
+			default:
+				cout << "Error! Enter a valid option number" << endl;
+				break;
+			}
+		} while (searchOption != 0);
+		
 		
 
-		cout << "Do you want to run program again?" << endl;
-		cout << "'y' = Yes" << endl;
-		cout << "'n' = No" << endl << endl;
-		cin >> option;
-		
-	}while (option == 'y');
+	cout << "\nDo you Want to run the program again?" << endl;
+	cout << "0. No" << endl;
+	cout << "1. Yes" << endl;
+	cin >> option;
+
+	}while (option != 0);
+
+	cout << "------------" << endl;
+	cout << "Program Exit" << endl;
+	cout << "------------" << endl;
 	return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,25 @@
-2
+case 2:
+		// 	int sortOption;
+		// 	do
+		// 	{
+		// 		cout << "\nSelect sorting method. Enter 0 to exit" << endl;
+		// 		cout << "1. Bubble Sort" << endl;
+		// 		cout << "2. Merge Sort" << endl << endl;
+		// 		cin >> sortOption;
+		// 		switch (sortOption)
+		// 		{
+		// 		case 0:
+		// 			break;
+				
+		// 		case 1:
+		// 			cout << "\nMerge Sort Selected" << endl;
+		// 			if ()
+		// 			{
+		// 				/* code */
+		// 			}
+					
+
+		// 		default:
+		// 			break;
+		// 		}
+		// 	} while (sortOption != 0);
